85fef74fffe7cf6712260bbced9fdb2302932c0c
RiccardoMarioBonato <Riccardomario.b@ku.th>
2024-03-12 17:58:51 +0700

    3 test cases

--- test_circle.py
+++ test_circle.py
@@ -8,5 +8,26 @@ Write unit tests as described in README.md.
 
 """
 from circle import Circle
+import unittest
 
-# TODO write 3 tests as described above
+
+class TestCircle(unittest.TestCase):
+    def test_add_area(self):
+        val1 = Circle(3)
+        val2 = Circle(4)
+        val3 = val1.add_area(val2)
+        self.assertEqual(val3.get_radius(), 5)
+
+    def test_area_edge_case(self):
+        val1 = Circle(3)
+        val2 = Circle(0)
+        val3 = val1.add_area(val2)
+        self.assertTrue(val3.get_radius(), 3)
+
+    def test_negative(self):
+        with self.assertRaises(ValueError):
+            Circle(-1)
+
+
+if __name__ == '__main__':
+    unittest.main()
